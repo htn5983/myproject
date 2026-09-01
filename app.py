a2d111b48ad83dc7fd706c4b9c4db2b1d0ad11a0
htn5983 <a0925072598@gmail.com>
2026-09-01 16:12:33 +0800

    加入 with st.sidebar

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import streamlit as st
 st.title("用Streamlit架設網站")
 
 #左側選單
-st.sidebar.title("選單")
-st.sidebar.write("這是選單的內容")
+#st.sidebar.title("選單")
+#st.sidebar.write("這是選單的內容")
+
+with st.sidebar:
+    st.title("選單標題")
+    st.write("選單內容")
+    st.button("按鈕A")
+    st.button("按鈕K")
 
 #網頁footer bottom聯絡資訊
-st.footer.header("聯絡資訊")
-st.footer.write("Email: example@gmail.com")
+st.bottom.header("聯絡資訊")
+st.bottom.write("Email: example@gmail.com")
